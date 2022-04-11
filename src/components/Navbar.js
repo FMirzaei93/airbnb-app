56aab7ca33f5622fd24ee4da2848a89d039234b1
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-04-11 08:08:39 -0700

    initial modifications

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import logo from "../images/airbnb-logo.png";
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      <img className='logo' src={logo} alt='logo image' />
+    </div>
+  );
+}
