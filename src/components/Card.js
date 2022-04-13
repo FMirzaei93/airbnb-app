27d3286658f3669893cee463280b7a97556a31a3
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-04-13 13:55:39 -0700

    restructuring process

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,34 @@
 import star from "../images/star.png";
 
-export default function Card(props) {
+export default function Card({ sold_status, location, price }) {
   return (
     <section className='card'>
       <div className='item-image'>
-        <img src={props.img} alt='item-image' />
-        <span className='sold-status'>{props.sold_status}</span>
+        {/* <span className='sold-status'>{props.sold_status}</span> */}
+        <span className='sold-status'>{sold_status}</span>
       </div>
       <div className='info'>
         <img src={star} alt='star-icon' />
-        <span className='ratings'>{props.rarings}</span>
-        <div className='ratings-num'>
-          <span>(</span>
-          {props.ratings_num}
-          <span>).</span>
-        </div>
-        <span className='location'>{props.location}</span>
+
+        {/* <span className='location'>{props.location}</span> */}
+        <span className='location'>{location}</span>
       </div>
-      <p className='title'>{props.title}</p>
-      <span className='price'>{props.price}</span>
+      {/* <span className='price'>{props.price}</span> */}
+      <span className='price'>{price}</span>
+
       <span className='owner-status'> {" / "}person</span>
     </section>
   );
 }
+
+// Note
+
+const card = {
+  ratings: "5",
+  location: "USA",
+  title: "I am the title",
+  price: "From $136",
+};
+
+const { ratings, title } = card;
+console.log(title);
