1834fc2f592d72b2dd45a90a811264854fc4a945
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-04-14 12:38:19 -0700

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ export default function Card(props) {
   return (
     <section className='card'>
       <div className='item-image'>
-        <img src={props.img} alt='item-image' />
+        <img src={`../images/${props.img}`} alt='item-image' />
         <span className='sold-status'>{props.sold_status}</span>
       </div>
       <div className='info'>
