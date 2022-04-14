a5066b873506e29ff5a0dfb47bf25289a6470e3f
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-04-14 16:11:53 -0700

    Create cardsData.js

---
+++ src/cardsData.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 0,
+    img: "katie-zaferes.png",
+    sold_status: "sold out",
+    ratings: "5",
+    ratings_num: 6,
+    location: "USA",
+    title: "Life lesson with Katie",
+    price: "From $136",
+  },
+  {
+    id: 1,
+    img: "katie-zaferes.png",
+    sold_status: "exists",
+    ratings: "4.5",
+    ratings_num: "30",
+    location: "USA",
+    title: "Jeans",
+    price: "From $50",
+  },
+];
