2da33eecce2d7e3cc7231360fcc82560329b8989
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-04-14 18:09:14 -0700

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,8 +1,7 @@
-import grid from "../images/photo-grid.png";
 export default function Intro() {
   return (
     <section className='Intro'>
-      <img src={grid} alt='grid image' />
+      <img src={"/images/photo-grid.png"} alt='grid image' />
       <h1>Online Experiences</h1>
       <p>
         Join unique interactive activities led by one-of-a-kind hosts—all
