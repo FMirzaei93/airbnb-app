9d31bd4dd33326b2c298383939d601e8eb5a7098
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-04-16 12:50:07 -0700

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,27 @@
 export default function Card(props) {
+  let badgeText;
+  props.item.openSpots > 0 ? (badgeText = "exist") : (badgeText = "sold out");
+
   return (
     <div className='card'>
       <div className='item-image'>
-        <img src={`/images/${props.img}`} alt='item-image' />
-
-        <span className='availability'>{props.availability}</span>
+        <img src={`/images/${props.item.coverImg}`} alt='item-image' />
+        {badgeText && <span className='availability'>{badgeText}</span>}
+        {/* The span will only be displayed if badgeText is true(if it exists)  */}
       </div>
       <div className='info'>
         <img src={"/images/star.png"} alt='star-icon' />
-        <span>{props.rating}</span>
+        <span>{props.item.stats.rating}</span>
         <div>
           <span>(</span>
-          {props.reviewCount}
-          <span>).</span>
+          {props.item.stats.reviewCount}
+          <span>) • </span>
         </div>
-        <span>{props.location}</span>
+        <span>{props.item.location}</span>
       </div>
-      <p className='description'>{props.description}</p>
+      <p className='description'>{props.item.description}</p>
       <div>
-        <span className='price'>{props.price}</span>{" "}
+        <span className='price'>{props.item.price}</span>{" "}
         <span style={{ fontSize: "10px" }}>{" / "}person</span>
       </div>
     </div>
